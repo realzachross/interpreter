ddb97a05edda8580394b1a1374add8226c7b2bf1
Zachary Ross <znr3@cornell.edu>
2022-04-26 17:19:00 -0400

    very basic calc interp done

--- calc1.py
+++ calc1.py
@@ -1,7 +1,7 @@
 INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
 
 
-class TOKEN(object):
+class Token(object):
     def __init__(self, type, value):
         self.type = type
         self.value = value
@@ -26,3 +26,47 @@ class Interpreter(object):
         text = self.text
         if self.pos > len(text) - 1:
             return Token(EOF, None)
+        current_char = text[self.pos]
+        if current_char.isdigit():
+            token = Token(INTEGER, int(current_char))
+            self.pos += 1
+            return token
+        if current_char == '+':
+            token = Token(PLUS, current_char)
+            self.pos += 1
+            return token
+        self.error()
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token()
+        left = self.current_token
+        self.eat(INTEGER)
+        op = self.current_token
+        self.eat(PLUS)
+        right = self.current_token
+        self.eat(INTEGER)
+        result = left.value + right.value
+        return result
+
+
+def main():
+    while True:
+        try:
+            text = input('calc: ')
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
